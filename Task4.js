e49e40b80320de267e4c2c3d53755fa44cb2e6eb
Windiana <krismanuyar@gmail.com>
2020-01-15 10:49:24 +0700

    Update Task4.js

--- Task4.js
+++ Task4.js
@@ -1,3 +1,6 @@
+// naming nya dibuat lebih rapih, kalo array bisa dinamain arr_name
 var name = ["john", "JACOB", "jinGleHeimer", "schmidt"]
+
+// parameter map jangan 'w' kaya gini, karena gak jelas. bisa dinamain name aja
 var cap = name.map(w => w.substring(0,1).toUpperCase()+w.substring(1).toLowerCase()).join(', ')
 console.log(cap)
